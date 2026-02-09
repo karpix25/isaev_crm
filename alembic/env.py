9cf8bee85353284fbfab2c480045c97c0f84ce88
karpix25 <nadaraya@gmail.com>
2026-02-09 17:01:13 -0300

    fix(alembic): escape percentage signs in connection string

--- alembic/env.py
+++ alembic/env.py
@@ -15,7 +15,7 @@ from src.config import settings
 config = context.config
 
 # Override sqlalchemy.url with the one from settings
-config.set_main_option("sqlalchemy.url", settings.database_url)
+config.set_main_option("sqlalchemy.url", settings.database_url.replace("%", "%%"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
