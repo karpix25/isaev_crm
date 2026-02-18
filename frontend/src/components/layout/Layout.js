ca1e64c145bfce61363f6851425f4e999c215403
karpix25 <nadaraya@gmail.com>
2026-02-18 20:44:20 -0300

    chore: remove stale JS files colliding with TSX source

--- frontend/src/components/layout/Layout.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import { Sidebar } from './Sidebar';
-import { Header } from './Header';
-export function Layout({ title }) {
-    return (_jsxs("div", { className: "flex h-screen overflow-hidden", children: [_jsx(Sidebar, {}), _jsxs("div", { className: "flex flex-1 flex-col overflow-hidden", children: [_jsx(Header, { title: title }), _jsx("main", { className: "flex-1 overflow-hidden bg-background p-6", children: _jsx(Outlet, {}) })] })] }));
-}
