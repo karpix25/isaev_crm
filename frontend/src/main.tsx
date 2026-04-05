ab7c2b020cadd620b5e2b5c2c9a805536b12d13a
karpix25 <nadaraya@gmail.com>
2026-04-04 21:55:09 -0300

    fix: redirect to https automatically to support Telegram Widget CSP

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+// Force HTTPS in production
+if (window.location.protocol === 'http:' && window.location.hostname !== 'localhost') {
+  window.location.replace(`https://${window.location.hostname}${window.location.pathname}${window.location.search}`);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
